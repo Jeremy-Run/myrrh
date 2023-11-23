47e9b57fb8ed71c603b54a747c800c870fbb8c12
Jeremy <acm_123@163.com>
2023-11-23 17:42:09 +0800

    add main

--- main.go
+++ main.go
@@ -1,1 +1,12 @@
-package myrrh
+package main
+
+import (
+	"fmt"
+	"myrrh/schedule"
+)
+
+func main() {
+	req := schedule.Requirement{}
+	executeResult := req.Execute()
+	fmt.Printf("The execute result is: %t \n", executeResult)
+}
